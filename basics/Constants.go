f8d65f011c069b9bae23497bd16d0d5bddda33b2
Vakil C. Arora <vakilarora@gmail.com>
2023-04-29 18:26:41 +0530

    Constants

---
+++ basics/Constants.go
@@ -0,0 +1,16 @@
+package main
+
+// exported constants - Note constant named starting with Capital letters
+const Pi = 3.142
+const One = 1
+
+// Not exported constants - Note names starting with lowercase letters
+
+const myPlace = "Bangalore"
+
+func main() {
+	const x = 10
+
+	print("\nPi = ", Pi, " One = ", One, " myPlace = ", myPlace, "\n\n")
+
+}
