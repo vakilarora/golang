012a292cfe47874e7b63bb9c92d1928e5e311743
Vakil C. Arora <vakilarora@gmail.com>
2023-04-29 17:10:12 +0530

    MultipleVariables

---
+++ basics/MultipleVariables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	a = 10
+	b = 12
+
+	fmt.Print(a, b)
+	fmt.Println("")
+
+	var c, d float32 = 100.0, 120.0
+
+	fmt.Print(c, d)
+	fmt.Println("")
+
+	e, f := "vakil ", "michelle"
+
+	fmt.Print(e, f)
+	fmt.Println("")
+
+}
